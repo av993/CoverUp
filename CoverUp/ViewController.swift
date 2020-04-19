0d74d3517552fd603861df615c2877788c78719b
Ameya Vaidya <ameyavaidya@ameyas-air.lan>
2020-04-19 15:42:39 -0400

    first commit

---
+++ CoverUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoverUp
+//
+//  Created by Ameya Vaidya on 4/19/20.
+//  Copyright © 2020 Ameya Vaidya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
